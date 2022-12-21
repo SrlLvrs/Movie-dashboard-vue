979af93bdf36a95fa9d871b980bee856b0f7ee76
Israel Olivares <iolivares0505@gmail.com>
2022-12-21 12:24:43 -0300

    pruebas navbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
  //import HelloWorld from './components/HelloWorld.vue'
+ import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
+  <NavBar></NavBar>
   <h1 class="text-3xl font-bold underline">
     Hello world!
   </h1>
