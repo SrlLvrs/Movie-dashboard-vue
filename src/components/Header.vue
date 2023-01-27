184b44baa1228fc76b1ca3bad1e26cc56f1b0c72
SrlLvrs <iolivares0505@gmail.com>
2023-01-27 18:45:48 -0300

    carousel flowbite

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,88 @@
 <script setup>
-// variable
-const msg = 'Hello!'
 
-// functions
-function log() {
-  console.log(msg)
-}
 </script>
 
 <template>
-  <button class="button" @click="log">{{ msg }}</button>
+  <div id="controls-carousel" class="relative" data-carousel="slide">
+    <!-- Carousel wrapper -->
+    <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+      <!-- Item 1 -->
+      <div class="duration-700 ease-in-out" data-carousel-item="active">
+        <img
+          src="../assets/foto-1.jpg"
+          class="absolute top-1/2 left-1/2 block w-full -translate-x-1/2 -translate-y-1/2"
+          alt="..."
+        />
+      </div>
+      <!-- Item 2 -->
+      <div class="duration-700 ease-in-out" data-carousel-item>
+        <img
+          src="../assets/foto-2.jpg"
+          class="absolute top-1/2 left-1/2 block w-full -translate-x-1/2 -translate-y-1/2"
+          alt="..."
+        />
+      </div>
+      <!-- Item 3 -->
+      <div class="duration-700 ease-in-out" data-carousel-item>
+        <img
+          src="../assets/foto-3.jpg"
+          class="absolute top-1/2 left-1/2 block w-full -translate-x-1/2 -translate-y-1/2"
+          alt="..."
+        />
+      </div>
+    </div>
+    <!-- Slider controls -->
+    <button
+      type="button"
+      class="group absolute top-0 left-0 z-30 flex h-full cursor-pointer items-center justify-center px-4 focus:outline-none"
+      data-carousel-prev
+    >
+      <span
+        class="inline-flex h-10 w-10 items-center justify-center rounded-full bg-white/30 group-hover:bg-white/50 group-focus:outline-none group-focus:ring-4 group-focus:ring-white dark:bg-gray-800/30 dark:group-hover:bg-gray-800/60 dark:group-focus:ring-gray-800/70"
+      >
+        <svg
+          aria-hidden="true"
+          class="h-6 w-6 text-white dark:text-gray-800"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M15 19l-7-7 7-7"
+          ></path>
+        </svg>
+        <span class="sr-only">Previous</span>
+      </span>
+    </button>
+    <button
+      type="button"
+      class="group absolute top-0 right-0 z-30 flex h-full cursor-pointer items-center justify-center px-4 focus:outline-none"
+      data-carousel-next
+    >
+      <span
+        class="inline-flex h-10 w-10 items-center justify-center rounded-full bg-white/30 group-hover:bg-white/50 group-focus:outline-none group-focus:ring-4 group-focus:ring-white dark:bg-gray-800/30 dark:group-hover:bg-gray-800/60 dark:group-focus:ring-gray-800/70"
+      >
+        <svg
+          aria-hidden="true"
+          class="h-6 w-6 text-white dark:text-gray-800"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M9 5l7 7-7 7"
+          ></path>
+        </svg>
+        <span class="sr-only">Next</span>
+      </span>
+    </button>
+  </div>
 </template>
