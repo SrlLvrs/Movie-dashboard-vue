f1a1f87424071db6285511a75860781533c58194
Israel Olivares <iolivares0505@gmail.com>
2023-02-07 02:23:17 -0300

    Inicio de Mejor Evaluadas

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
  import NavBar from './components/NavBar.vue'
  import Destacadas from './components/Destacadas.vue'
  import SeguirViendo from './components/SeguirViendo.vue'
+import MejorEvaluadas from './components/MejorEvaluadas.vue';
 </script>
 
 <template>
   <NavBar></NavBar>
   <Destacadas></Destacadas>
   <SeguirViendo></SeguirViendo>
+  <MejorEvaluadas></MejorEvaluadas>
 </template>
 
 <style scoped>
